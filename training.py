c4ae25f4541ad3da3dc09f693197fe16be527949
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-08-31 21:26:17 +0300

    created the first initial version, training works

--- training.py
+++ training.py
@@ -5,23 +5,35 @@ from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3 import PPO
 import os
 
-log_path = os.makedirs("logs", exist_ok=True)
-model_path = os.makedirs("models", exist_ok=True)
+def train():
+    os.makedirs("logs", exist_ok=True)
+    os.makedirs("models", exist_ok=True)
+    timesteps = 1_000_000
+    
+    # TODO: vecnormalize these
+    train_env = make_vec_env("BipedalWalker-v3", 4, env_kwargs={"hardcore":True})
+    eval_env = DummyVecEnv([lambda: gym.make("BipedalWalker-v3", hardcore=True)])
+    
+    eval_callback = EvalCallback(
+        eval_env,
+        log_path="logs",
+        best_model_save_path="models"
+    )
+    
+    model = PPO(
+        "MlpPolicy",
+        train_env,
+        # the rest are gonna be default for now
+    )
+    
+    model.learn(
+        total_timesteps=timesteps,
+        callback=eval_callback,
+        progress_bar=True
+    )
 
-# TODO: vecnormalize these
-train_env = make_vec_env("BipedalWalker-v3", 4, env_kwargs={"hardcore":True})
-eval_env = DummyVecEnv([lambda: gym.make("BipedalWalker-v3", hardcore=True)])
+    model.save("models")
 
-eval_callback = EvalCallback(
-    eval_env,
-    log_path,
-    best_model_save_path=model_path
-)
-
-model = PPO(
-    "MlpPolicy",
-    train_env,
-    # the rest are gonna be default for now
-)
-
+if __name__ == "__main__":
+    train()
 
